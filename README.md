56189915a816ef51d3076db832995930f4716a42
CJ <chrishorlebein@gmail.com>
2020-08-25 17:18:00 -0400

    add updated read me

---
+++ README.md
@@ -0,0 +1,27 @@
+# job-scrapper
+
+
+> Generate job data on the fly for various cities
+> 
+> Note: This is meant to be a fun hobby project for tracking job fluctuations
+
+### Arthur: <a href="https://github.com/CJHorlebein" target="_blank">**CJ Horlebein**</a>
+
+---
+
+**this project users the following technologies**
+
+- Javascript
+- Node
+- Axios / Ajax
+
+## About:
+This project works by hitting the indeed job board and looking for the total records for a total of 30 cities for the relevant search term. It then compiles these into a simple CSV document and auto downloads them. 
+
+## Operation
+
+To use this program, please first clone this repo and then run `yarn` to install the relevant packages. Once you have done this, you can now run the script by typing `yarn run [keyword]` in the terminal where keyword is the term you wish to search by. A csv document containing the scrapped data will be downloaded. 
+
+---
+
+- Copyright 2019 © <a href="https://bountyfull.me" target="_blank">BountyFull</a>.
--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-const axios = require('axios')
-const locations = require('./data.json')
+const axios = require('axios');
+const locations = require('./data.json');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const searchResultsId = 'Page 1 of '
 
 const fetchJobData = async (url) => {
-    const { data } = await axios.get(url)
-    const pos = data.indexOf(searchResultsId)
-    return data.slice(pos + 7, pos + 17).replace(/\D+/g, "")
-}
+  const { data } = await axios.get(url);
+  const pos = data.indexOf('Page 1 of ');
+  return data.slice(pos + 7, pos + 17).replace(/\D+/g, '');
+};
 
 const FormatJobData = async (local) => {
-    const jobUrl = `https://www.indeed.com/jobs?q=javascript&l=${local}`
-    return {
-        city: local.split('%2C+')[0],
-        qty: await fetchJobData(jobUrl),
-    }
-}
+  const keyword = process.argv[2];
+  const jobUrl = `https://www.indeed.com/jobs?q=${keyword}&l=${local}`;
+  return {
+    city: local.split('%2C+')[0],
+    qty: await fetchJobData(jobUrl),
+  };
+};
 
-const compileJobsData = () => (Promise.all(locations.map(FormatJobData)))
+const compileJobsData = () => Promise.all(locations.map(FormatJobData));
 
 const generateCsvJobsData = async () => {
-    const csvName = `jobs-data-${new Date().getTime()}.csv`
-    const jobsData = await compileJobsData()
-    const csvWriter = createCsvWriter({
-        path: csvName,
-        header: [
-            { id: 'city', title: 'City' },
-            { id: 'qty', title: 'Qty' },
-        ]
+  const csvName = `jobs-data-${new Date().getTime()}.csv`;
+  const jobsData = await compileJobsData();
+  const csvWriter = createCsvWriter({
+    path: csvName,
+    header: [
+      { id: 'city', title: 'City' },
+      { id: 'qty', title: 'Qty' },
+    ],
+  });
+  csvWriter
+    .writeRecords(jobsData)
+    .then(() => console.log(`${csvName} created successfully`))
+    .catch(() => {
+      console.log('Failed to write jobs data to file.');
     });
-    csvWriter
-        .writeRecords(jobsData)
-        .then(() => console.log(`${csvName} created successfully`))
-        .catch(() => { })
-}
+};
 
-generateCsvJobsData()
+generateCsvJobsData();
